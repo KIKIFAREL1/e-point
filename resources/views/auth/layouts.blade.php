1fd562e1ec7ac5780ba1662b2c797e03205cd10f
faizdhya <faiz.rayadhya2007@smkn4-tsm.sch.id>
2024-12-02 13:27:41 +0700

    aplikasi epoin

---
+++ resources/views/auth/layouts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE htlm>
+<html lang="en">
+
+<head>
+<meta charset="UFT-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-compatible" content="ie=edge">
+<title>Laravel 11 </title>
+</head>
+
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+
+</html>
